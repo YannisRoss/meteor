9170dc471a67de810d680f7ff413c1a9f1163487
Rossiadis Yiannis <81157620+YiannisRoss@users.noreply.github.com>
2023-08-06 00:39:05 +0300

    17 delete stored weather data (#19)
    * Add StoredWeatherData deletion
    
    * Add title to weather data objects for easier selection
    
    ---------

--- app/controllers/weather_data_controller.rb
+++ app/controllers/weather_data_controller.rb
@@ -19,8 +19,8 @@ class WeatherDataController < ApplicationController
   def create
     if has_resume?
       if permitted_params[:weather_data].present?
-
-        data = StoredWeatherDataObject.new(user: Current.user, contents: permitted_params[:weather_data].to_json)
+        data = StoredWeatherDataObject.new(user: Current.user, title: permitted_params[:title],
+                                           contents: permitted_params[:weather_data].to_json)
 
         if data.save
           render json: data, status: :ok
@@ -37,10 +37,20 @@ class WeatherDataController < ApplicationController
     end
   end
 
+  def destroy
+    data = StoredWeatherDataObject.find(params[:id])
+    if data.user == Current.user
+      data.destroy
+      render json: Current.user.stored_weather_data_objects, status: :ok
+    else
+      render json: "You don't have permission to delete other users' data", status: :unauthorized
+    end
+  end
+
   private
 
   def permitted_params
-    params.permit(:latitude, :longitude, weather_data: %i[time temperature])
+    params.permit(:latitude, :longitude, :title, weather_data: %i[time temperature])
   end
 
   def has_resume?
