c194c0168fe375f258b563a5d9e908b08e7ec1fc
Rossiadis Yiannis <81157620+YiannisRoss@users.noreply.github.com>
2023-08-06 19:20:48 +0300

    20 api documentation (#21)
    * Install rswag
    
    * Configure API doc yaml
    
    ---------

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   post 'sign_in', to: 'sessions#create'
 
   resources :users, only: :create
